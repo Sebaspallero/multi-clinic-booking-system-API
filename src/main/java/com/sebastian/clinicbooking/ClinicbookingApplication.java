64301159865d23f3a0c141e145ba2ad8d6853b3e
Sebastian Pallero <sebastianpallerodev@gmail.com>
2025-04-30 17:24:22 -0300

    Feat: Initial Project Config

---
+++ src/main/java/com/sebastian/clinicbooking/ClinicbookingApplication.java
@@ -0,0 +1,21 @@
+package com.sebastian.clinicbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+    info = @Info(title = "Multi Clinic Booking System API", version = "1.0", description = "API for booking appointments in multiple clinics"),
+    servers = @Server(url = "http://localhost:8080")
+)
+@SpringBootApplication
+public class ClinicbookingApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicbookingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sebastian/clinicbooking/ClinicbookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastian.clinicbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicbookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
